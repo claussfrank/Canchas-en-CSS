e0f0f8ed09b861ff19bf0b00f07d16fb1df2ad6e
Clauss Franco <Clauss Franco>
2017-04-11 14:53:09 -0500

    Interaccion Canchas y Botones

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,19 @@ var botonBasket = document.getElementById('basketBall');
 var botonTenis = document.getElementById('tenis');
 
 botonFut.addEventListener("click",mostarCanchaFut);
+botonFut.addEventListener("dblclick",ocultarCanchaFut);
 botonBasket.addEventListener("click",mostarCanchaBasquet);
+botonBasket.addEventListener("dblclick",ocultarCanchaBasquet);
 botonTenis.addEventListener("click",mostarCanchaTenis);
-
+botonTenis.addEventListener("dblclick",ocultarCanchaTenis);
 
 function mostarCanchaFut(){
  var mostrarFootball = document.getElementById('canchaFootball');
  mostrarFootball.style.display="block";
+}
+function ocultarCanchaFut(){
+ var ocultarFootball = document.getElementById('canchaFootball');
+ ocultarFootball.style.display="none";
 }
 
 function mostarCanchaBasquet(){
@@ -17,7 +23,15 @@ function mostarCanchaBasquet(){
  mostrarBasket.style.display="block";
 }
 
+function ocultarCanchaBasquet(){
+ var ocultarBasket = document.getElementById('canchaBasketBall');
+ ocultarBasket.style.display="none";
+}
 function mostarCanchaTenis(){
  var mostrarTenis = document.getElementById('canchaTenis');
  mostrarTenis.style.display="block";
+}
+function ocultarCanchaTenis(){
+ var ocultarTenis = document.getElementById('canchaTenis');
+ ocultarTenis.style.display="none";
 }
